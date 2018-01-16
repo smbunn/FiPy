cb0504fca0ccb60e2f0a2dc8f9ac1844a5d288fa
smbunn <sbunn@derceto.com>
2018-01-16 15:31:58 +1300

    Adding LoRa to PySense

--- sense.py
+++ sense.py
@@ -1,5 +1,6 @@
 # See https://docs.pycom.io for more information regarding library specifics
-
+from network import LoRa
+#import config
 from pysense import Pysense
 from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
@@ -18,8 +19,35 @@ green = 0x00ff00
 blue = 0x0000ff
 yellow = 0xffff00
 
+# Initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# Set up KotahiNet channels
+lora.add_channel(0, frequency=864862500, dr_min=0, dr_max=5)
+lora.add_channel(1, frequency=865062500, dr_min=0, dr_max=5)
+lora.add_channel(2, frequency=865402500, dr_min=0, dr_max=5)
+lora.add_channel(3, frequency=865602500, dr_min=0, dr_max=5)
+lora.add_channel(4, frequency=865985000, dr_min=0, dr_max=5)
+lora.add_channel(5, frequency=866200000, dr_min=0, dr_max=5)
+lora.add_channel(6, frequency=866400000, dr_min=0, dr_max=5)
+lora.add_channel(7, frequency=866600000, dr_min=0, dr_max=5)
+
 # Turn off hearbeat LED
 pycom.heartbeat(False)
+
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify('00 70 C3 4D'.replace(' ','')))[0]
+nwk_swkey = binascii.unhexlify('1BFE9C81AD35B39019244D155CE31D88'.replace(' ',''))
+app_swkey = binascii.unhexlify('045ABBEFE974D3E257AC31522E8E837B'.replace(' ',''))
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
 
 for count in range (2000):
     py = Pysense()
@@ -27,7 +55,8 @@ for count in range (2000):
     si = SI7006A20(py)
     lt = LTR329ALS01(py)
     li = LIS2HH12(py)
-
+    s.setblocking(True)
+    print("Count=", count)
     print("MPL3115A2 temperature: " + str(mp.temperature()))
     print("Altitude: " + str(mp.altitude()))
     mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa. Mode may also be set to ALTITUDE, returning a value in meters
@@ -43,7 +72,7 @@ for count in range (2000):
     print("Acceleration: " + str(li.acceleration()))
     print("Roll: " + str(li.roll()))
     print("Pitch: " + str(li.pitch()))
-
+    voltage = py.read_battery_voltage
     print("Battery voltage: " + str(py.read_battery_voltage()))
     pycom.rgbled(blue)
     time.sleep(0.5)
@@ -52,4 +81,13 @@ for count in range (2000):
     pycom.rgbled(red)
     time.sleep(0.5)
     pycom.rgbled(off)
-    time.sleep(20)  # wait time between packets sent
+    s.send(buffer)  # send buffer to TTN
+
+    #s.send(bytes([0x01, 0x02, 0x03, 0x04]))
+    print('data sent')
+    time.sleep(0.5)
+    # get any data received&
+    s.setblocking(False)
+    data = s.recv(64)
+    time.sleep(0.5)
+    print(data)  #anything received?time.sleep(20)  # wait time between packets sent
